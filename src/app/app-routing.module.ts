17e7ee4d46bc457256e7156d6aa62f63903f7a0c
varunkondreddy <45779365+varunkondreddy@users.noreply.github.com>
2020-04-27 12:37:33 +0530

    made compatible for website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule), canActivate: [AuthGuardService]},
   { path: 'floors', loadChildren: './tab1/floors/floors.module#FloorsPageModule' },
   { path: 'taps', loadChildren: './tab1/floors/taps/taps.module#TapsPageModule' },
-  { path: 'compform', loadChildren: './tab2/compform/compform.module#CompformPageModule' },
+  { path: 'compform', loadChildren: './tab2/compform/compform.module#CompformPageModule' }, 
   // { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
   { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
